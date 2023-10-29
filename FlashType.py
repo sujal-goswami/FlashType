870af0531b04a192bcc85523a2f1b6b81089d141
sumitjhaleriya <jhaleriyasumit@gmail.com>
2023-10-29 19:13:51 +0530

    Added New Ascii Design on Welcome Screen

--- FlashType.py
+++ FlashType.py
@@ -26,17 +26,43 @@ art = """
                                                      \/           
 """
 
+
 def start_screen(stdscr):
     stdscr.clear()
-    # stdscr.addstr(0, 0, art, curses.A_BOLD)
-    stdscr.addstr(curses.LINES // 2 - 5 - 2  , curses.COLS // 2 - 5, "Welcome to")
-    stdscr.addstr(curses.LINES // 2 - 3 - 2, curses.COLS // 2 - 5, "Flash⚡Type!", curses.color_pair(5))
-    # stdscr.addstr(curses.LINES // 2, curses.COLS // 2 - 5, "")
-    stdscr.addstr(curses.LINES // 2 - 1 - 2, curses.COLS // 2 - 11, "Press any key to start")
-    stdscr.addstr(curses.LINES // 2 - 1 , 5, art, curses.color_pair(5))
+    custom_banner = """
+    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    @______  _       ___   _____  _   _   _____ __   ________  _____ @
+    @|  ___|| |     / _ \ /  ___|| | | | |_   _|\ \ / /| ___ \|  ___|@
+    @| |_   | |    / /_\ \\ `--. | |_| |   | |   \ V / | |_/ /| |__  @
+    @|  _|  | |    |  _  | `--. \|  _  |   | |    \ /  |  __/ |  __| @
+    @| |    | |____| | | |/\__/ /| | | |   | |    | |  | |    | |___ @
+    @\_|    \_____/\_| |_/\____/ \_| |_/   \_/    \_/  \_|    \____/ @
+    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    """
+    custom_banner_lines = custom_banner.strip().split('\n')
+    banner_height = len(custom_banner_lines)
+    banner_width = max(len(line) for line in custom_banner_lines)
+
+    start_x = (curses.COLS - banner_width) // 2
+    start_y = (curses.LINES - banner_height) // 2
+
+    for i, line in enumerate(custom_banner_lines):
+        stdscr.addstr(start_y + i, start_x, line)
+
+    # Add a custom title
+    title = "Flash⚡Type"
+    title_x = (curses.COLS - len(title)) // 2
+    stdscr.addstr(start_y + banner_height + 1, title_x,
+                  title, curses.color_pair(5) | curses.A_BOLD)
+
+    # Additional information or instructions can be added here
+    instructions = "Press any key to start"
+    instructions_x = (curses.COLS - len(instructions)) // 2
+    stdscr.addstr(start_y + banner_height + 3,
+                  instructions_x, instructions, curses.A_BOLD)
 
     stdscr.refresh()
-    stdscr.getkey()
+    stdscr.getch()
 
 
 def load_text(desired_length=300):
@@ -54,7 +80,7 @@ def load_text(desired_length=300):
         with open('Text.txt', 'r') as f:
             accumulated_text = f.read().replace('\n', ' ')
     return accumulated_text
-     
+
 
 def wrap_text(text, width):
     wrapped_text = textwrap.wrap(text, width)
@@ -68,11 +94,13 @@ def display_text(stdscr, Type_win, target_text, current_text, wpm=0, accuracy=0)
     for i, line in enumerate(target_text):
         Type_win.addstr(2+i, 5, line)
 
-    Type_win.addstr(Type_win_height - 2, Type_win_width // 2 - 10, f"WPM: {wpm}")
-    Type_win.addstr(Type_win_height - 2, Type_win_width // 2 , f"Accuracy: {accuracy}%")
+    Type_win.addstr(Type_win_height - 2, Type_win_width //
+                    2 - 10, f"WPM: {wpm}")
+    Type_win.addstr(Type_win_height - 2, Type_win_width //
+                    2, f"Accuracy: {accuracy}%")
 
     target_text = ";".join(target_text)
-    
+
     col = 5
     indx = 0
 
@@ -80,13 +108,13 @@ def display_text(stdscr, Type_win, target_text, current_text, wpm=0, accuracy=0)
         if target_text[i] == ";":
             col = 5
             indx += 1
-        else:    
+        else:
             if char == target_text[i]:
                 Type_win.addstr(indx+2, col, char, curses.color_pair(1))
             else:
-                Type_win.addstr(indx+2, col, char, curses.color_pair(2))    
+                Type_win.addstr(indx+2, col, char, curses.color_pair(2))
             col += 1
-            
+
 
 def typing_test(stdscr, typing_window):
     target_text = load_text()
@@ -102,14 +130,15 @@ def typing_test(stdscr, typing_window):
         time_elapsed = max(time.time() - start_time, 1)
         wpm = round(len(current_text) / 5 / time_elapsed * 60)
         if len(current_text) > 0:
-            correct = sum([1 for i in range(len(current_text)) if current_text[i] == target_text[i]])
+            correct = sum([1 for i in range(len(current_text))
+                          if current_text[i] == target_text[i]])
             accuracy = round(correct / len(current_text) * 100)
-
 
         if len(current_text) == len(target_text):
             return wpm, accuracy
 
-        display_text(stdscr, typing_window, target_text, current_text, wpm , accuracy)
+        display_text(stdscr, typing_window, target_text,
+                     current_text, wpm, accuracy)
         typing_window.refresh()
 
         if len(current_text) == len(target_text):
@@ -123,7 +152,7 @@ def typing_test(stdscr, typing_window):
 
         if key == chr(27):
             return wpm, accuracy
-        
+
         if key in ("KEY_ENTER", "\n"):
             current_text.append("\n")
 
@@ -132,17 +161,20 @@ def typing_test(stdscr, typing_window):
                 current_text.pop()
 
         elif len(current_text) < len(target_text):
-             current_text.append(key)    
+            current_text.append(key)
 
 
 def result_screen(stdscr, typing_window, wpm, accuracy):
     height, width = typing_window.getmaxyx()
     typing_window.clear()
     typing_window.border()
-    typing_window.addstr(2  , width // 2 - 3, "Results", curses.color_pair(4))
-    typing_window.addstr(height - 5, width // 2 - 11, f"WPM: {wpm}", curses.A_STANDOUT)
-    typing_window.addstr(height - 5, width // 2 + 1, f"Accuracy: {accuracy}%", curses.A_STANDOUT)
-    typing_window.addstr(height - 2, curses.COLS // 2 - 25, "Press Space bar to start again 🔁")
+    typing_window.addstr(2, width // 2 - 3, "Results", curses.color_pair(4))
+    typing_window.addstr(height - 5, width // 2 - 11,
+                         f"WPM: {wpm}", curses.A_STANDOUT)
+    typing_window.addstr(height - 5, width // 2 + 1,
+                         f"Accuracy: {accuracy}%", curses.A_STANDOUT)
+    typing_window.addstr(height - 2, curses.COLS // 2 - 25,
+                         "Press Space bar to start again 🔁")
     typing_window.refresh()
 
     typing_window.nodelay(1)  # Set typing window to non-blocking mode
@@ -152,6 +184,7 @@ def result_screen(stdscr, typing_window, wpm, accuracy):
             return True
         elif key != -1:  # Check if any other key was pressed
             return False
+
 
 def main(stdscr):
     content = load_text()
@@ -166,16 +199,19 @@ def main(stdscr):
     while True:
         stdscr.clear()
         stdscr.refresh()
-        stdscr.addstr(5 , curses.COLS // 2 - 5, "Flash⚡Type", curses.color_pair(5))
+        stdscr.addstr(5, curses.COLS // 2 - 5,
+                      "Flash⚡Type", curses.color_pair(5))
         stdscr.refresh()
-        Typing_window = curses.newwin(curses.LINES - 20, curses.COLS - 20, 10, 10)
+        Typing_window = curses.newwin(
+            curses.LINES - 20, curses.COLS - 20, 10, 10)
         Typing_window.border()
-        wpm , accuracy = typing_test(stdscr, Typing_window)
+        wpm, accuracy = typing_test(stdscr, Typing_window)
         retry = result_screen(stdscr, Typing_window, wpm, accuracy)
         if retry:
             continue
         else:
             break
+
 
 if __name__ == "__main__":
     wrapper(main)
